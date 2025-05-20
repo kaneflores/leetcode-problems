76acd14142576580fe4b4fe85ff3f7adf30cdf2a
Kane Flores <kaneflores911@gmail.com>
2025-05-20 17:48:13 -0400

    added two sum solution

---
+++ example.py
@@ -0,0 +1,9 @@
+# Two sum solution: #############################################################################
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for x in range(0, len(nums)):
+            for y in range(x+1, len(nums)):
+                if nums[x]+nums[y] == target:
+                    return [x,y]
+        return []
+#######BRUTE FORCE###############################################################
