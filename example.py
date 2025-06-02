3aa72f08f8213c871e56db2d0ce73e4993412c6b
Kane Flores <kaneflores911@gmail.com>
2025-06-02 16:39:11 -0400

    added valid palindrome part 1

--- example.py
+++ example.py
@@ -87,3 +87,16 @@ class Solution:
                 curr_profit = price - lowest
         return curr_profit
 ################################################################################################################################
+#
+
+## valid palindrome solution#####
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newstr = ""
+        for c in s:
+            if c.isalnum():
+                newstr = newstr + c.lower()
+        return newstr == newstr[::-1]
+    
+ 
+################################################################################################################################
