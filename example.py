506bf8397418f896681584e4adcaed47738ae543
Kane Flores <kaneflores911@gmail.com>
2025-06-02 14:36:59 -0400

    added merge two sorted lists problem

--- example.py
+++ example.py
@@ -45,3 +45,29 @@ class Solution:
         # After processing all characters, the stack should be empty if all brackets are matched
         return True if not stack else False
 #######O(n) solution################################################################1
+
+# merge two sorted lists solution: #############################################################################
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1:# not empty yet, but there are not enough values to analyze
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+        return dummy.next
+#######O(n) solution################################################################1
