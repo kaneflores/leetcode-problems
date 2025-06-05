8815953bf067d28b34bd7a2d1c2700d863f9f83d
Kane Flores <kaneflores911@gmail.com>
2025-06-04 21:15:34 -0400

    added anagram part 2

--- example.py
+++ example.py
@@ -151,4 +151,19 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
 
-    
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        #create hashmap to count letters from both
+        countS, countT = {}, {}
+
+        # fill up hashmaps by adding the letters and incremental values into the hashmap
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0) 
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        # compare the counts of each character
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True 
