41ce468b6d6a3ea48fcb294dca3b9361348fe266
Kane Flores <kaneflores911@gmail.com>
2025-06-25 13:47:39 -0400

    perfected binary search

--- example.py
+++ example.py
@@ -192,16 +192,17 @@ class Solution:
 ######################BINARY SEARCH
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        low = nums[0]
-        high = nums[-1]
-        while low < high:
-            mid = low + (high - low)//2
+        l, r = 0, len(nums)-1
 
-            if nums[mid] == target:
-                return mid
+        while l<=r:
+            m = (l+r) // 2
 
-            elif nums[mid]<target:
-                low = mid +1
-            else:
-                high = mid -1
-        return -1 
+            if nums[m] > target:
+                #target is on right half portion of the nums list
+                r = m-1
+            elif nums[m] < target:
+                #target is on left portion of the list
+                l = m+1
+            else:# nums[m] == target
+                return m
+        return -1 #failure of search
