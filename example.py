07765df538b7348313b22d9eec66e52e9bcb1d8e
kaneflores <93720236+kaneflores@users.noreply.github.com>
2026-07-20 20:39:59 -0400

    Add twoSum method to Solution class

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 # Two sum solution: #############################################################################
 class Solution:
+    
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for x in range(0, len(nums)):
             for y in range(x+1, len(nums)):
