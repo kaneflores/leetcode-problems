0243844c8a053671e4e901079f33f67066dfd497
kaneflores <93720236+kaneflores@users.noreply.github.com>
2026-07-20 20:40:30 -0400

    Update example.py

--- example.py
+++ example.py
@@ -5,6 +5,7 @@ class Solution:
         for x in range(0, len(nums)):
             for y in range(x+1, len(nums)):
                 if nums[x]+nums[y] == target:
+                    
                     return [x,y]
                     
                     
